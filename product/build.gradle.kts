f40c1f7091303e01d4f001ede9edf5ca2a800b49
pranav029 <pranavmani.naman@gmail.com>
2026-05-09 21:30:42 +0530

    Add entity classes for product management: Attribute, Category, Product, Variant, and AbstractEntity

--- product/build.gradle.kts
+++ product/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
     compileOnly("org.projectlombok:lombok")
